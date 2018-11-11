f18a135e32a544ae1ca56bd9da990ba5331a8f08
Hyun <dr.moon.kr@gmail.com>
2018-11-11 23:11:03 +0900

    settings.py was changed.

--- sa_server/settings.py
+++ sa_server/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SERVICE_CONFIGURATIONS = {
     'SERVER_TYPE': 'global',
